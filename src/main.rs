7e4643c99960aa6bff8bde6c07d2574371605957
ks-rjjones <rjjones@keysysconsulting.com>
2023-10-13 15:12:40 -0500

    use semver from lock file

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ mod error;
 mod toml;
 
 use clap::Parser;
-
 use std::process::Command;
 
 fn main() -> Result<(), String> {
@@ -15,35 +14,17 @@ fn main() -> Result<(), String> {
             .map_err(|e| e.to_string());
     }
 
-    let dependencies = toml::parse_toml();
+    let dependencies = toml::get_dependencies();
 
-    for (name, dep) in dependencies {
-        match dep {
-            toml::Dependency::Simple(version) => {
-                println!("Building {}:{}", name, version);
-                let _ = command::execute_command(
-                    &mut Command::new("cargo")
-                        .arg("build")
-                        .arg("--package")
-                        .arg(format!("{}:{}", name, version).as_str()),
-                )
-                .map_err(|e| e.to_string());
-            }
-            toml::Dependency::Detailed(details) => {
-                let version = details
-                    .version
-                    .expect(format!("no version specified for {}", name).as_str());
-                println!("Building {}:{}", name, version);
-                let _ = command::execute_command(
-                    &mut Command::new("cargo")
-                        .arg("build")
-                        .arg("--package")
-                        .arg(format!("{}:{}", name, version).as_str()),
-                )
-                .map_err(|e| e.to_string());
-            }
-            _ => (),
-        }
+    for dep in dependencies {
+        println!("Building {}", dep);
+        let _ = command::execute_command(
+            &mut Command::new("cargo")
+                .arg("build")
+                .arg("--package")
+                .arg(dep.as_str()),
+        )
+        .map_err(|e| e.to_string());
     }
 
     Ok(())
--- src/toml.rs
+++ src/toml.rs
@@ -1,20 +1,27 @@
 extern crate cargo_toml;
 
+use cargo_lock::Lockfile;
 pub use cargo_toml::Dependency;
 use cargo_toml::Manifest;
-use std::{collections::BTreeMap, path::Path};
+use std::path::Path;
 
-pub fn parse_toml() -> BTreeMap<String, Dependency> {
+pub fn get_dependencies() -> Vec<String> {
+    let lockfile = Lockfile::load("Cargo.lock").unwrap();
+    let package_names = get_dependency_names_from_toml();
+    let mut dependencies: Vec<String> = Vec::with_capacity(128);
+
+    for p in &lockfile.packages {
+        if package_names.contains(&p.name.to_string()) {
+            dependencies.push(format!("{}@{}", p.name, p.version));
+        }
+    }
+
+    dependencies
+}
+
+fn get_dependency_names_from_toml() -> Vec<String> {
     let path = Path::new("Cargo.toml");
     let manifest = Manifest::from_path(&path).unwrap();
     let dependencies = &manifest.dependencies;
-
-    // for (name, dep) in dependencies {
-    //     match dep {
-    //         Dependency::Simple(version) => println!("{}:{}", name, version),
-    //         _ => (),
-    //     }
-    // }
-
-    dependencies.clone()
+    dependencies.iter().map(|(name, _)| name.clone()).collect()
 }
